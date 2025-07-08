f0c8ca558b62d3789be41698149cc7c1b160a6a6
Erick Loayza <jeloech@icloud.com>
2025-07-08 15:29:36 -0500

    actualizamos contacto

--- src/app/components/contacto/contacto.component.ts
+++ src/app/components/contacto/contacto.component.ts
@@ -2,10 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-contacto',
+  standalone: true,
   imports: [],
   templateUrl: './contacto.component.html',
   styleUrl: './contacto.component.css'
 })
 export class ContactoComponent {
-
+  enviarFormulario(event: Event) {
+    event.preventDefault();
+    alert('✅ Mensaje enviado correctamente (simulado)');
+  }
 }
