125c93af416ded233c2713b4350e7e24476ad3b4
Erick Loayza <jeloech@icloud.com>
2025-07-08 12:14:36 -0500

    actualizamos portafolio modales

--- src/app/components/portafolio/portafolio.component.ts
+++ src/app/components/portafolio/portafolio.component.ts
@@ -6,6 +6,16 @@ import { Component } from '@angular/core';
   templateUrl: './portafolio.component.html',
   styleUrl: './portafolio.component.css'
 })
-export class PortafolioComponent {
 
+export class PortafolioComponent {
+  mostrarModal = false;
+
+  abrirModal() {
+    this.mostrarModal = true;
+  }
+
+  cerrarModal() {
+    this.mostrarModal = false;
+  }
 }
+
